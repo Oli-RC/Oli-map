982bfe4e060f3520fb3c518c67d10f1288e12acd
Oli-RC <oromerocortell@veepee.com>
2021-09-21 17:19:03 +0200

    open house roulette

---
+++ asset open house_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="asset open house_1" tilewidth="32" tileheight="32" tilecount="75" columns="3">
+ <image source="src/assets/asset open house_1.png" width="96" height="800"/>
+</tileset>
